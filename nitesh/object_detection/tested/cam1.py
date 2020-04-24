690c7f7dab9bd392412fcb9849e23ef4534529fa
Nitesh Sonwani <projectblackcat101@gmail.com>
2020-04-24 23:22:09 +0530

    m

--- nitesh/object_detection/tested/cam1.py
+++ nitesh/object_detection/tested/cam1.py
@@ -1,18 +1,19 @@
 import numpy as np
 import cv2
-
+count = 1
 def main():
     cap = cv2.VideoCapture(0)
     cv2.namedWindow('frame')
-    count = 1
+    
 
     while(True):
         ret, frame = cap.read()
         cv2.setMouseCallback('frame',captureFrame,frame)
+        print('hi')
         
 
         if ret:
-            cv2.imshow('frame',frame)
+            #cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -22,6 +23,7 @@ def main():
 def captureFrame(event,x,y,flags,frame):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         cv2.imwrite('test.jpg',frame) # want to save frame here
+        exit()
 
 
 if __name__ == "__main__":
